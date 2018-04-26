10fefd5b3373c11386211d5cb3d4cc92143d162c
Anders Reiche <andersreiche@gmail.com>
2018-04-26 11:39:08 +0200

    finished task 1 logic

--- BIS.ino
+++ BIS.ino
@@ -1,11 +1,19 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
+const int resistorPin = A0;
+const int delaytime = 1;
+int resistorValue = 0;
+
+
 // Set the LCD I2C address
 LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 
 int task1(void) {
-  if (1) { // Should test if analogRead(resistorPin) is between two set values
+  resistorValue = analogRead(resistorPin);
+  Serial.println(resistorValue);
+  if (resistorValue > 344 && resistorValue < 444) { // Should test if analogRead(resistorPin) is between two set values
+    Serial.println("Done!");
     return 0;
   }
 }
@@ -23,65 +31,66 @@ int task3(void) {
 }
 
 int task4(void) {
-/*
- * Analog read value should be written to the display
- * Some timing functionality is needed here (maybe milis()) to test if value is stable for 2 seconds
- */
-  
+  /*
+     Analog read value should be written to the display
+     Some timing functionality is needed here (maybe milis()) to test if value is stable for 2 seconds
+  */
+
   if (1) {// Should test if analogRead(voltagePin) is between two set values
     //start/restart the milis() timer
     if (1) { // if milis() > 2000 && analogRead(voltagePin) is between two set values
-     return 0; 
+      return 0;
     }
   }
 }
 
 void setup() {
-lcd.begin(16, 2);  // initialize the lcd
-lcd.clear();
+  Serial.begin(9600);
+  lcd.begin(16, 2);  // initialize the lcd
+  lcd.clear();
 }
 
 void loop() {
-lcd.setCursor(0, 0);
-lcd.print("Welcome");
-lcd.setCursor(0, 1);
-lcd.print("...");
-delay(5);
+  lcd.setCursor(0, 0);
+  lcd.print("Welcome");
+  lcd.setCursor(0, 1);
+  lcd.print("...");
+  delay(delaytime);
 
-lcd.setCursor(0, 0);
-lcd.print("Task 1");
-while (task1() != 0) {} // Wait here till task1 is completed
-lcd.setCursor(0, 1);
-lcd.print("Completed!");
-delay(5);
-lcd.setCursor(0, 1);
-lcd.print("...!");
+  lcd.setCursor(0, 0);
+  lcd.print("Task 1");
+  while (task1() != 0) {} // Wait here till task1 is completed
+  lcd.setCursor(0, 1);
+  lcd.print("Completed!");
+  delay(delaytime);
+  lcd.setCursor(0, 1);
+  lcd.print("...!");
 
-lcd.setCursor(0, 0);
-lcd.print("Task 2");
-while (task2() != 0) {} // Wait here till task2 is completed
-lcd.setCursor(0, 1);
-lcd.print("Completed!");
-delay(5);
-lcd.setCursor(0, 1);
-lcd.print("...!");
+  lcd.setCursor(0, 0);
+  lcd.print("Task 2");
+  while (task2() != 0) {} // Wait here till task2 is completed
+  lcd.setCursor(0, 1);
+  lcd.print("Completed!");
+  delay(delaytime);
+  lcd.setCursor(0, 1);
+  lcd.print("...!");
 
-lcd.setCursor(0, 0);
-lcd.print("Task 3");
-while (task3() != 0) {} // Wait here till task3 is completed
-lcd.setCursor(0, 1);
-lcd.print("Completed!");
-delay(5);
-lcd.setCursor(0, 1);
-lcd.print("...!");
+  lcd.setCursor(0, 0);
+  lcd.print("Task 3");
+  while (task3() != 0) {} // Wait here till task3 is completed
+  lcd.setCursor(0, 1);
+  lcd.print("Completed!");
+  delay(delaytime);
+  lcd.setCursor(0, 1);
+  lcd.print("...!");
 
 
-lcd.setCursor(0, 0);
-lcd.print("Task 4");
-while (task4() != 0) {} // Wait here till task4 is completed
-lcd.setCursor(0, 1);
-lcd.print("Completed!");
-delay(5);
-lcd.setCursor(0, 1);
-lcd.print("...!");
+  lcd.setCursor(0, 0);
+  lcd.print("Task 4");
+  while (task4() != 0) {} // Wait here till task4 is completed
+  lcd.setCursor(0, 1);
+  lcd.print("Completed!");
+  delay(delaytime);
+  lcd.setCursor(0, 1);
+  lcd.print("...!");
 }
